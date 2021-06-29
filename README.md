d1c0beeeb491428b0b898b0f4a448c86c5755d08
gengxiaojun <gengxiaojun@zto.com>
2021-06-29 11:01:42 +0800

    upload: 修改命令,add: 增加层级创建目录

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
-<!-- 暂无 -->
-
 # cli
 
 释放双手，愉快,高效的工作
@@ -22,20 +20,11 @@
   // 此部分还未实现
 
   // 创建模板
-  ['工具名'] create <project-name>
-
-  // 创建模板 存放路径
-  ['工具名'] create <project-name>  -d <'存放路径'>
-
-  // 创建模板 选择框架
-  ['工具名'] create <project-name>  -t <'框架名称'>
+  ['工具名'] create <project-name> || ['工具名'] create <project-name> [otherDirs...] || -t <'框架名称'>
 
   // 如果是clone 到本地，启动方式则 把工具名替换成
-  npm test create <project-name> ['后面参数如上图']
+  npm test create <project-name> [otherDirs...] -t <'框架名称'> ||  npm test create <project-name>
 
-
+  // 查看版本
+  npm test -V
 ```
-
-#### 查看版本
-
-> -V |--version
--- bin/index.js
+++ bin/index.js
@@ -1,7 +1,6 @@
 #!/usr/bin/env node
 
 const semver = require('semver')
-const program = require('commander');
 const chalk = require('chalk');
 const package = require('../package.json')
 const log = console.log
@@ -19,21 +18,21 @@ function checkNodeVersion(wanted, packageName) {
 // 检查node 版本
 checkNodeVersion(package.engines.node,package.name)
 
-
+const program = require('commander');
 program.version(`${package.name} ${package.version}`).usage('<command> [options]')
 
+
 program
-.command('create <app-name>')
-.description('create a new project demo')
-.option('-d, --dir <dirName>','Project Installation Path')
-.option('-t, --template <templateFramework>','Use that template framework')
-.action((name, options) => {
-  if(require('minimist')(process.argv.slice(3))._.length > 1) {
+.command('create <projectName> [otherDirs...]')
+.description('To create a project, you can specify the frame and directory')
+.option('-t, --template <framework>')
+.action((name,dir,options) => {
+  if(require('minimist')(process.argv.slice(3))._.length > 2) {
     log(chalk.yellow('\n Info: You provided more than one argument. The first one will be used as the app\'s name, the rest are ignored.'))
     log(chalk.yellow(' 您提供了多个参数。第一个将被用作应用程序的名称，其余的将被忽略。'))
   }
 
-  require('../lib/create')(name, options)
+  require('../lib/create')(name,dir, options)
 })
 
 program.on('command:*', function () {
