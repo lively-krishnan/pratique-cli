0f832679be6f526eb2ec962e0456d3e875f64faa
gengxiaojun <gengxiaojun@zto.com>
2021-07-14 16:42:06 +0800

    upload: 修复命令行问题

--- apa/src/router/index.js
+++
@@ -1,9 +0,0 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
-import routes from './modules/index.js'
-
-const router = createRouter({
-  history: createWebHashHistory(),
-  routes,
-})
-
-export default router
--- apa/src/router/modules/index.js
+++
@@ -1,7 +0,0 @@
-import route from './demo_route'
-
-const RouteList = [
-  ...route
-]
-
-export default RouteList
